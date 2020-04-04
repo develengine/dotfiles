69b46838fc794958626a9abe61fbc896a4c172ba
Jakub Boros <enginedevel@gmail.com>
2020-04-04 22:35:32 +0200

    dwm start.

--- collect.sh
+++ collect.sh
@@ -8,4 +8,6 @@ cp ~/.vimrc $to
 cp ~/.config/compton.conf $to
 cp ~/.bashrc $to
 cp ~/apps/st/config.h $to
+cp ~/apps/dwm/dwm/config.h $to
 cp -r ~/.scripts $to
+cp ~/onstart.sh $to
---
+++ onstart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+exec xwallpaper --zoom ~/Pictures/paps/doomy.png
+exec compton --config ~/.config/compton.conf -b
